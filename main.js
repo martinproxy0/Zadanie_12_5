f265cd813e367229bc678a6b87f3d93f4aa9ca8a
martinproxy0 <marcinsornek@gmail.com>
2017-12-25 12:28:27 +0100

    Fix CORS problem.

--- main.js
+++ main.js
@@ -1,3 +1,4 @@
+var prefix = "https://cors-anywhere.herokuapp.com/";
 var tweetLink = "https://twitter.com/intent/tweet?text=";
 var quoteUrl = "https://quotesondesign.com/wp-json/posts?filter[orderby]=rand&filter[posts_per_page]=1";
 
@@ -10,7 +11,8 @@ $(function() {
 });
 
 function getQuote() {
-    $.getJSON(quoteUrl, createTweet);
+    $.getJSON(prefix + quoteUrl, createTweet);
+    $.ajaxSetup({ cache: false });
 }
 
 function createTweet(input) {
